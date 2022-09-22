23ee2d1e28f5fd8a9e08d9bfd4526667d311dc1a
Macoix <giusemedina@gmail.com>
2022-09-22 10:56:19 -0500

    Adding assets and icons to filters

--- src/const/filters.ts
+++ src/const/filters.ts
@@ -1,14 +1,21 @@
+import tinyReact from "../../assets/tiny-react.png";
+import tinyAngular from "../../assets/tiny-angular.png";
+import tinyVue from "../../assets/tiny-vue.png";
+
 export const FILTERS = [
   {
     name: "Angular",
     value: "angular",
+    logo: tinyAngular
   },
   {
     name: "Vue",
     value: "vuejs",
+    logo: tinyVue
   },
   {
     name: "React",
     value: "reactjs",
+    logo: tinyReact
   }
 ];
