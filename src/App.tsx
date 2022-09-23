d52717c825d649b9b3baea83e7f5f070043b9891
Macoix <giusemedina@gmail.com>
2022-09-23 16:03:40 -0500

    changing logic and adding new components

--- src/App.tsx
+++ src/App.tsx
@@ -2,47 +2,55 @@ import React, { ChangeEvent, useEffect, useState } from 'react';
 import { validateHits } from './helpers/validateHits';
 import { Hit } from './interfaces/hit';
 import { FILTERS } from "./const/filters";
-import { Filters } from "./interfaces/filters";
+import { Filter } from "./interfaces/filters";
+import hitFetch from './helpers/hitsFetch';
 import Card from "./components/card";
 import Select from "./components/select";
+import Pagination from './components/pagination';
+import Loader from './components/loader';
+
 
 import './assets/styles/app.scss';
 
 function App() {
-  const [hits, setHits] = useState<Hit[]>([]);
   const [query, setQuery] = useState<string>("");
   const [page, setPage] = useState<number>(0);
   const [favorites, setFavorites] = useState<Hit[]>([]);
   const [switchValue, setSwitchValue] = useState<string>("all");
   const [currentSelected, setCurrentSelected] = useState("Select your news");
 
+  const { hits, totalPages, hitsPerPage, error, loading } = hitFetch(query, page);
+
   useEffect(() => {
+    //check if the filter is in the localStore
     const filter = localStorage.getItem("filters")
       ? localStorage.getItem("filters")
       : "";
+
+    //check if the filter name is in the localStotage
+    const filterName = localStorage.getItem("filterName")
+      ? localStorage.getItem("filterName")
+      : "";
+
+    //bring the favorites hits from the local store and save it in a constnt
+    let fav = JSON.parse(localStorage.getItem("favorites") as string) as Hit[];
+
+    //check if fav is empty and reinicialize the variable
+    if (!fav) fav = [];
+    setFavorites(fav);
     setQuery(filter as string);
+    setCurrentSelected(filterName as string);
   }, []);
 
-  useEffect(() => {
-    const fecthData = () => {
-      fetch(
-        `https://hn.algolia.com/api/v1/search_by_date?query=${query}&page=${page}`
-      )
-        .then((response) => response.json())
-        .then((data: { hits: Hit[] }) => {
-          setHits(data.hits.filter((hit) => validateHits(hit)));
-        });
-    };
-    fecthData();
-  }, [query, page]);
-
-  const handleSelectChange = (filter: Filters) => {
+  //Function that handle select option event click.
+  const handleSelectChange = (filter: Filter) => {
     localStorage.setItem("filters", filter.value);
     localStorage.setItem("filterName", filter.name);
     setQuery(filter.value);
     setCurrentSelected(filter.name);
   };
 
+  //Function handle to click on heart icon button
   const handleLikeBtn = (hit: Hit) => {
     let fav = JSON.parse(localStorage.getItem("favorites") as string) as Hit[];
     if (!fav) fav = [];
@@ -58,10 +66,12 @@ function App() {
     localStorage.setItem("favorites", JSON.stringify(fav));
   };
 
+  //Function that checks witch element is selected in favorites and returns true if it is.
   const checkFavorites = (id: string) => {
     return favorites.some((i) => i.objectID === id);
   };
 
+  //Function that handle the event change in the switch component.
   const handleSwitchEvent = (event: React.MouseEvent<HTMLElement>) => {
     console.log(event.currentTarget.dataset.value)
     setSwitchValue(event.currentTarget.dataset.value as string);
@@ -76,20 +86,20 @@ function App() {
       </header>
 
       <div className="tabs">
-          <button
-            className={`tab-item all ${switchValue === "all" ? "active" : ""}`}
-            onClick={handleSwitchEvent}
-            data-value="all"
-          >
-            All
-          </button>
-          <button
-            className={`tab-item myfaves ${switchValue === "myFaves" ? "active" : ""}`}
-            onClick={handleSwitchEvent}
-            data-value="myFaves"
-          >
-            My faves
-          </button>
+        <button
+          className={`tab-item all ${switchValue === "all" ? "active" : ""}`}
+          onClick={handleSwitchEvent}
+          data-value="all"
+        >
+          All
+        </button>
+        <button
+          className={`tab-item myfaves ${switchValue === "myFaves" ? "active" : ""}`}
+          onClick={handleSwitchEvent}
+          data-value="myFaves"
+        >
+          My faves
+        </button>
       </div>
 
       <div className="container">
@@ -102,41 +112,57 @@ function App() {
         )}
       </div>
       <div className="container">
-        <div className="items-wrapper">
-          {switchValue === "all" &&
-            hits.map((hit, i) => {
-              return (
-                <Card
-                  key={i}
-                  hit={{
-                    author: hit.author,
-                    created_at: hit.created_at,
-                    story_title: hit.story_title,
-                    story_url: hit.story_url,
-                    objectID: hit.objectID
-                  }}
-                  handleLikeBtn={handleLikeBtn}
-                  liked={checkFavorites(hit.objectID)}
-                />
-              )
-          })}
-          {switchValue === "myFaves" &&
-            favorites.map((fav, i) => (
-              <Card
-                key={i}
-                hit={{
-                  author: fav.author,
-                  created_at: fav.created_at,
-                  story_title: fav.story_title,
-                  story_url: fav.story_url,
-                  objectID: fav.objectID
-                }}
-                handleLikeBtn={handleLikeBtn}
-                liked={checkFavorites(fav.objectID)}
+        {
+          !loading ? (
+            <>
+
+              <div className="items-wrapper">
+                {switchValue === "all" &&
+                  hits.map((hit, i) => {
+                    return (
+                      <Card
+                        key={i}
+                        hit={{
+                          author: hit.author,
+                          created_at: hit.created_at,
+                          story_title: hit.story_title,
+                          story_url: hit.story_url,
+                          objectID: hit.objectID
+                        }}
+                        handleLikeBtn={handleLikeBtn}
+                        liked={checkFavorites(hit.objectID)}
+                      />
+                    )
+                })}
+                {switchValue === "myFaves" &&
+                  favorites.map((fav, i) => (
+                    <Card
+                      key={i}
+                      hit={{
+                        author: fav.author,
+                        created_at: fav.created_at,
+                        story_title: fav.story_title,
+                        story_url: fav.story_url,
+                        objectID: fav.objectID
+                      }}
+                      handleLikeBtn={handleLikeBtn}
+                      liked={checkFavorites(fav.objectID)}
+                    />
+                ))}
+              </div>
+              <Pagination
+                totalPages={totalPages}
+                page={page}
+                setPage={setPage}
               />
-          ))}
-        </div>
+            </>
+          ) : (
+            <Loader />
+          )
+        }
       </div>
+
+
     </>
   );
 }
